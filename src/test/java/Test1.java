4d7804b65910e53ca2af8a2e0c479b708c316479
sgianashvili <sofio.gianashvili@gmail.com>
2021-01-07 11:27:44 +0400

    Hmework5

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -1,8 +1,14 @@
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.conditions.Visible;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import static com.codeborne.selenide.Selenide.$;
+
+import static com.codeborne.selenide.Selectors.*;
+
 
 public class Test1 {
     @Test
@@ -11,12 +17,20 @@ public class Test1 {
         Configuration.baseUrl = "https://www.google.com/";
         System.setProperty("webdriver.chrome.driver", "chrome/chromedriver.exe");
         Configuration.startMaximized = true;
-        Selenide.open("https://www.google.com/");
-        Thread.sleep(3000);
-        $(".gLFyf").setValue("Tako").sendKeys(Keys.ENTER);
-        Thread.sleep(5000);
+        Selenide.open("https://redmed.ge/ka");
+        $(byXpath("//*[@id=\"header\"]/div/div/div[3]/div[3]/div/b")).click();
+        $(".login-link-main-page").click();
+        //$(By.tagName("h1")).$(byText("შესვლა")).shouldBe(Condition.visible);
+        $("#telLogin").setValue("571001205").sendKeys(Keys.ENTER);
+        $(byText("მონაცემები არასწორია")).shouldBe(Condition.visible);
+
+
+
+
+
 
 
     }
+
 
 }
